cc9e3ab8ed2fe9ca58b3eeb284529767fc4f5974
rutika <63110959+rutika-dhumal@users.noreply.github.com>
2022-07-22 11:34:44 +0530

    Add files via upload

---
+++ FunctionTypeTest.java
@@ -0,0 +1,46 @@
+
+public class FunctionTypeTest {
+	
+	public static void main(String[] args) {
+		
+		
+		Addition addObj = new Addition();
+		addObj.add();
+		addObj.addInt(10, 34);
+		double floatAdd = addObj.addFloat(34.0f, 50.65f);
+		System.out.println(floatAdd);
+		double ans = addObj.add4();
+		System.out.println(ans);
+	}
+
+}
+
+
+
+class Addition
+{
+	void add() 
+	{
+		System.out.println("Addition");
+		System.out.println("---------------------");
+	}
+	
+	
+	void addInt(int x,int y) {
+		System.out.println("Addition is : "+ x+y);
+		System.out.println("---------------------");
+	}
+	
+	double addFloat(float a , float b) 
+	{
+		System.out.println("---------------------");
+		return a+b;
+		
+	}
+	
+	double add4() 
+	{
+		System.out.println("---------------------");
+		return 34.689 + 56.87;
+	}
+}
