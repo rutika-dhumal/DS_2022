e44a25de8610e493884679b03129e27a1e41270c
rutika <63110959+rutika-dhumal@users.noreply.github.com>
2022-08-03 17:36:14 +0530

    Add files via upload

---
+++ StudentAlreadyExistsException.java
@@ -0,0 +1,7 @@
+
+public class StudentAlreadyExistsException extends Exception {
+  public StudentAlreadyExistsException(String msg) {
+	// TODO Auto-generated constructor stub
+	  super(msg);
+ }
+}
